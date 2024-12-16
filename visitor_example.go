ffcb4df6dbfe52be7017f483b8d6a11c1e088a42
l0nm4r <l0nm4r@163.com>
2024-12-17 00:50:49 +0800

    feat: adjust example visitor

--- visitor_example.go
+++ visitor_example.go
@@ -11,11 +11,11 @@ type ExampleVisitor struct{}
 
 func (v ExampleVisitor) Enter(n ast.Node) IVisitor {
 	nodeType := reflect.TypeOf(n)
-	fmt.Printf("enter node: \n\t%s,\n \ttype: %s\n", "n", nodeType)
+	fmt.Printf("enter node: \n---- ---- ----\n\t%s,\n\t[type: %s]\n", n, nodeType)
 	return v
 }
 
 func (v ExampleVisitor) Exit(n ast.Node) {
 	nodeType := reflect.TypeOf(n)
-	fmt.Printf("leaving node: %s, type: %s", "n", nodeType)
+	fmt.Printf("leaving node: \n---- ---- ----\n\t%s,\n\t[type: %s]\n", n, nodeType)
 }
